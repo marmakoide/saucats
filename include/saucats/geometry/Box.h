dc62971a9cb3cda557a976802a58652381b55826
Alexandre Devert <alex_devert@adagene.cn>
2018-11-03 21:11:01 +0100

    Added the Box and Sphere classes

---
+++ include/saucats/geometry/Box.h
@@ -0,0 +1,111 @@
+#ifndef SAUCATS_GEOMETRY_BOX_H
+#define SAUCATS_GEOMETRY_BOX_H
+
+#include <Eigen/Dense>
+
+
+
+namespace saucats {
+	/*
+	 * The BoxT class represents a N-dimensional axis-aligned box
+	 */
+
+	template <class VectorT>
+	class BoxT {
+	public:
+		typedef typename VectorT::Scalar scalar_type;
+		typedef VectorT vector_type;
+
+		static constexpr unsigned int CORNER_COUNT = 1 << vector_type::RowsAtCompileTime;
+
+
+
+		BoxT(const vector_type& extent = vector_type::Zero(),
+		     const vector_type& center = vector_type::Zero()) :
+			m_half_extent(extent / 2),
+			m_center(center) { } 
+
+		inline BoxT(const BoxT& box) :
+			m_half_extent(box.m_half_extent),
+			m_center(box.m_center) { }
+
+		inline BoxT& operator = (const BoxT& box) {
+			m_half_extent = box.m_half_extent;
+			m_center = box.m_center;
+			return *this;
+		}
+
+		// Returns the center of the box
+		inline vector_type& center() {
+			return m_center;
+		}
+
+		// Returns the center of the box
+		inline const vector_type& center() const {
+			return m_center;
+		}
+
+		// Returns the size of the box on each dimension, divided by two
+		inline const vector_type& half_extent() const {
+			return m_half_extent;
+		}
+
+		// Returns the corner with the lowest coordinates
+		inline vector_type min_corner() const {
+			return m_center - m_half_extent;
+		}
+
+		// Returns the corner with the highest coordinates
+		inline vector_type max_corner() const {
+			return m_center + m_half_extent;
+		}
+
+		// Returns true if a point is inside the box
+		inline bool contains(const vector_type& X) const {
+			return ((X - m_center).cwiseAbs() - m_half_extent).maxCoeff() < 0;
+		}
+
+		// Returns the signed Euclidean distance from a point to the box
+		scalar_type signed_dist(const vector_type& X) const {
+			vector_type M = ((X - m_center).cwiseAbs() - m_half_extent);
+	
+			scalar_type max_coeff = M.maxCoeff();
+			if (max_coeff < 0)
+				return max_coeff;
+	
+			return M.cwiseMax(0).norm();
+		}
+
+		// Returns the corners of the box
+		Eigen::Matrix<scalar_type, CORNER_COUNT, vector_type::RowsAtCompileTime> corners() const {
+			Eigen::Matrix<scalar_type, CORNER_COUNT, vector_type::RowsAtCompileTime> ret;
+			for(unsigned int i = 0; i < CORNER_COUNT; ++i)
+				for(Eigen::Index j = 0; j < vector_type::RowsAtCompileTime; ++j)
+					ret(i, j) = i & (1 << j) ? m_half_extent(j) : -m_half_extent(j);
+
+			ret.rowwise() += m_center.transpose();
+	
+			return ret;
+		}
+
+		// Returns the volume (or surface area in 2d) of the box
+		inline scalar_type volume() const {
+			return std::pow(2, vector_type::RowsAtCompileTime) * m_half_extent.prod();
+		}
+
+	private:
+		vector_type m_half_extent;
+		vector_type m_center;
+	}; // class BoxT
+
+
+
+	typedef BoxT<Eigen::Vector2f> Box2f;
+	typedef BoxT<Eigen::Vector2d> Box2d;
+	typedef BoxT<Eigen::Vector3f> Box3f;
+	typedef BoxT<Eigen::Vector3d> Box3d;
+} // namespace saucats
+
+
+
+#endif // SAUCATS_GEOMETRY_BOX_H
---
+++ include/saucats/geometry/Sphere.h
@@ -0,0 +1,100 @@
+#ifndef SAUCATS_GEOMETRY_SPHERE_H
+#define SAUCATS_GEOMETRY_SPHERE_H
+
+#include <Eigen/Dense>
+
+
+
+namespace saucats {
+	/*
+	 * The SphereT class represents a N-dimensional sphere
+	 */
+
+	template <class VectorT>
+	class SphereT {
+	public:
+		typedef typename VectorT::Scalar scalar_type;
+		typedef VectorT vector_type;
+
+
+
+		inline SphereT() :
+			m_radius_sqr(0),
+			m_center(vector_type::Zero()) { }
+
+		inline SphereT(scalar_type radius) :
+			m_radius_sqr(radius * radius),
+			m_center(vector_type::Zero()) { }
+
+		inline SphereT(const vector_type& center,
+		               scalar_type radius) :
+			m_radius_sqr(radius * radius),
+			m_center(center) { }
+
+		inline SphereT(const vector_type& center) :
+			m_radius_sqr(0),
+			m_center(center) { }
+
+		inline SphereT(const SphereT& sphere) :
+			m_radius_sqr(sphere.m_radius_sqr),
+			m_center(sphere.m_center) { }
+
+		inline SphereT& operator = (const SphereT& sphere) {
+			m_radius_sqr = sphere.m_radius_sqr;
+			m_center = sphere.m_center;
+			return *this;
+		}
+
+		// Returns the radius of the sphere
+		inline scalar_type radius() const {
+			return std::sqrt(m_radius_sqr);
+		}
+
+		// Returns the square of the radius of the sphere
+		inline scalar_type squared_radius() const {
+			return m_radius_sqr;
+		}
+
+		// Returns the center of the sphere
+		inline vector_type& center() {
+			return m_center;
+		}
+
+		// Returns the center of the sphere
+		inline const vector_type& center() const {
+			return m_center;
+		}
+
+		// Returns the square of the Euclidean distance from a point to the sphere
+		scalar_type squared_dist(const vector_type& X) const {
+			double delta_sqr_norm = (m_center - X).squaredNorm();
+			return delta_sqr_norm + m_radius_sqr - 2 * std::sqrt(delta_sqr_norm * m_radius_sqr);
+		}
+
+		// Returns the signed Euclidean distance from a point to the sphere
+		scalar_type signed_dist(const vector_type& X) const {
+			double delta_sqr_norm = (m_center - X).squaredNorm();
+			return std::copysign(std::sqrt(delta_sqr_norm) - std::sqrt(radius), delta_sqr_norm - m_radius_sqr);
+		}
+		
+		// Returns true if a point is inside the sphere
+		inline bool contains(const vector_type& X) const {
+			return (X - m_center).squaredNorm() <= m_radius_sqr;
+		}
+
+	private:
+		scalar_type m_radius_sqr;
+		vector_type m_center;
+	}; // class SphereT
+
+
+
+	typedef SphereT<Eigen::Vector2f> Sphere2f;
+	typedef SphereT<Eigen::Vector2d> Sphere2d;
+	typedef SphereT<Eigen::Vector3f> Sphere3f;
+	typedef SphereT<Eigen::Vector3d> Sphere3d;
+} // namespace saucats
+
+
+
+#endif // SAUCATS_GEOMETRY_SPHERE_H
