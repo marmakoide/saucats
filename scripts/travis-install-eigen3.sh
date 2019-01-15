3d96700a8eef5ff13e744acf3be9652c8498344d
Alexandre Devert <alex_devert@adagene.cn>
2019-01-15 14:07:32 +0100

    Fixed Travis continuous integration : install Eigen3 to /usr

--- scripts/travis-install-eigen3.sh
+++ scripts/travis-install-eigen3.sh
@@ -10,5 +10,5 @@ rm 3.3.7.tar.bz2
 cd eigen-eigen-323c052e1731
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/local
-make install
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr
+sudo make install
