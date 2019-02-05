ad66f1a9e1a1793fe05266bd36e127fda8cdaceb
Alexandre Devert <alex_devert@adagene.cn>
2019-02-05 09:55:39 +0100

    Fixed method name in SphereSDF

--- include/saucats/sdf/SphereSDF.h
+++ include/saucats/sdf/SphereSDF.h
@@ -33,7 +33,7 @@ namespace saucats {
 			return (m_sphere.center() - X).norm() - m_radius;
 		}
 
-		inline sphere_type bounding_sphere() const {
+		inline sphere_type get_bounding_sphere() const {
 			return get_bounding_sphere(m_sphere);
 		}
 
