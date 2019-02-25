043fe15e180c44b1a25a099147cfdf2063b0a953
Alexandre Devert <alex_devert@adagene.cn>
2019-02-25 22:13:29 +0100

    Made PhongFragmentShader a generic type

--- include/saucats/render/PhongFragmentShader.h
+++ include/saucats/render/PhongFragmentShader.h
@@ -7,14 +7,18 @@
 
 
 namespace saucats {
-	class PhongFragmentShader {
+	template <class ScalarT>
+	class PhongFragmentShaderT {
 	public:
-		typedef Eigen::Vector3d vector_type;
-		typedef Eigen::Vector3d color_type;
+		typedef ScalarT scalar_type;
+		typedef Eigen::Matrix<scalar_type, 3, 1> vector_type;
+		typedef Eigen::Matrix<scalar_type, 3, 1> color_type;
+		typedef LineT<vector_type> line_type;
+		typedef SphereT<vector_type> sphere_type;
 
 
 
-		PhongFragmentShader() :
+		PhongFragmentShaderT() :
 			m_light_dir(vector_type(-.2, .2, 1.).normalized()) { }
 
 		inline vector_type& light_dir() {
@@ -26,26 +30,26 @@ namespace saucats {
 		}
 
 		template <class func_type>
-		double
+		scalar_type
 		get_shadow(const func_type& sdf,
-		           const Line3d& line,
-		           const typename func_type::sphere_type& bounding_sphere,
-		           double k = 2) const {
-			const double tol = 1e-6;
+		           const line_type& line,
+		           const sphere_type& bounding_sphere,
+		           scalar_type k = 2) const {
+			const scalar_type tol = 1e-6;
 	
 			auto inter = get_sphere_line_intersection(bounding_sphere, line);
-			double t_max = inter.tmax();
+			scalar_type t_max = inter.tmax();
 
-			double res = 1.0;
-			double ph = 1e20;
-			for(double t = 1e-4; t < t_max; ) {
-				double h = sdf.dist(line.point(t));
+			scalar_type res = 1;
+			scalar_type ph = 1e20;
+			for(scalar_type t = 1e-4; t < t_max; ) {
+				scalar_type h = sdf.dist(line.point(t));
 				if (h < tol)
 					return 0.;
 
-				double y = h * h / (2. * ph);
-				double d = std::sqrt(h * h - y * y);
-				res = std::fmin(res, k * d / std::fmax(0., t - y));
+				scalar_type y = h * h / (2 * ph);
+				scalar_type d = std::sqrt(h * h - y * y);
+				res = std::fmin(res, k * d / std::fmax(scalar_type(0), t - y));
 				ph = h;
 				t += h;
 			}
@@ -56,37 +60,34 @@ namespace saucats {
 		template <class func_type>
 		color_type
 		get_color(const func_type& sdf,
-		          const Line3d& eye_line,
-		          const typename func_type::sphere_type& bounding_sphere,
-		          double dist) const {
-			typedef typename func_type::vector_type vector_type;
-
+		          const line_type& eye_line,
+		          const sphere_type& bounding_sphere,
+		          scalar_type dist) const {
 			// Intersection position
 			vector_type P = eye_line.point(dist);
 
 			// Shadow
-			double u = get_shadow(sdf, Line3d(-m_light_dir, P), bounding_sphere);
+			scalar_type u = get_shadow(sdf, line_type(-m_light_dir, P), bounding_sphere);
 
 			// Normal vector
 			vector_type N = get_sdf_gradient(sdf, P, 1e-6).normalized();
 
 			// Light
 			vector_type L = m_light_dir;
-			double l = std::fmax(0, -N.dot(L));
-			vector_type color(1., .7, 0.);
+			scalar_type l = std::fmax(0, -N.dot(L));
+			color_type color;
+			color << 1., .7, 0.;
 			return std::fmax(u * l, .1) * color; //m_texture.get_color(P);
 		}
 
 		private:
 			vector_type m_light_dir;
-	}; // class PhongFragmentShader
+	}; // class PhongFragmentShaderT
 
 
 
-	PhongFragmentShader
-	get_phong_fragment_shader() {
-		return PhongFragmentShader();
-	}
+	typedef PhongFragmentShaderT<float> PhongFragmentShaderf;
+	typedef PhongFragmentShaderT<double> PhongFragmentShaderd;
 } // namespace saucats
 
 
