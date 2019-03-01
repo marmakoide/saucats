e391b6464c5f3da3437329ec7755a873216b7f70
Alexandre Devert <alex_devert@adagene.cn>
2019-03-01 21:13:31 +0100

    In trimesh example, bumped resolution for 512x512, changed object pose and print render time

--- examples/trimesh/Main.cpp
+++ examples/trimesh/Main.cpp
@@ -14,16 +14,16 @@ int
 main(int UNUSED_PARAM(argc), char** UNUSED_PARAM(argv)) {
 	// Load triangle mesh
 	std::vector<TriMeshSDF<double>::triangle_type> triangle_list;
-	STLParser<double>::load("./data/L-shape.stl", std::back_inserter(triangle_list));
+	STLParser<double>::load("./data/fox.stl", std::back_inserter(triangle_list));
 	
 	// Setup the distance field
 	auto sdf = get_sdf_rotation3(
 		get_trimesh_sdf(triangle_list),
-		Eigen::AngleAxisd(M_PI / 6., Eigen::Vector3d(0., -1., 0.))
+		Eigen::AngleAxisd(M_PI / 2., Eigen::Vector3d(0., -1., 0.)) * Eigen::AngleAxisd(M_PI / 2., Eigen::Vector3d(1., 0., 0.))
 	);
 
 	// Setup the render target
-	PNGRenderTarget render_target("out.png", 256, 256);
+	PNGRenderTarget render_target("out.png", 512, 512);
 
 	// Setup the shader
 	PerspectiveProjectiond projection;
@@ -35,7 +35,9 @@ main(int UNUSED_PARAM(argc), char** UNUSED_PARAM(argv)) {
 	auto shader = get_isosurface_3d_shader(sdf, frag_shader, bg_shader, projection);
 
 	// Render the distance field
-	get_renderer(shader, render_target).render();
+	auto renderer = get_renderer(shader, render_target);
+	renderer.render();
+	cout << "render time = " << renderer.render_time() << endl;
 
 	// Job done
 	return EXIT_SUCCESS;
