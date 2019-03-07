e29d6a260a0b11c17863f8227d865246a2c44900
Alexandre Devert <alex_devert@adagene.cn>
2019-03-07 13:30:42 +0100

    Added 1d real FFT routines

--- include/saucats/utils/FFT.h
+++ include/saucats/utils/FFT.h
@@ -13,6 +13,61 @@ namespace saucats {
 
 	class FFT {
 	public:
+		static Eigen::VectorXcd
+		real_fft_1d(const Eigen::VectorXd& A) {
+			// Allocate ressources
+			double* A_data = (double*)fftw_malloc(sizeof(double) * A.size());
+			fftw_complex* U_data = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * (1 + A.size() / 2));
+
+			// Mapping of raw data for Eigen library
+			Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, 1> > A_data_map(A_data, A.size());
+			Eigen::Map<Eigen::Matrix<std::complex<double>, Eigen::Dynamic, 1> > U_data_map((std::complex<double>*)U_data, 1 + A.size() / 2);
+	
+			// Setup FFT computations
+			fftw_plan fftw_plan = fftw_plan_dft_r2c_1d(A.size(), A_data, U_data, FFTW_ESTIMATE);
+
+			// Compute FFT
+			A_data_map = A;
+			fftw_execute(fftw_plan);
+			Eigen::VectorXcd U = U_data_map;
+
+			// Free ressources	
+			fftw_destroy_plan(fftw_plan);
+			fftw_free(U_data);
+			fftw_free(A_data);
+
+			// Job done
+			return U;
+		}
+
+		static Eigen::VectorXd
+		real_ifft_1d(const Eigen::VectorXcd& U) {
+			// Allocate ressources
+			double* A_data = (double*)fftw_malloc(sizeof(double) * (2 * (U.size() - 1)));
+			fftw_complex* U_data = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * U.size());
+
+			// Mapping of raw data for Eigen library
+			Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, 1> > A_data_map(A_data, 2 * (U.size() - 1));
+			Eigen::Map<Eigen::Matrix<std::complex<double>, Eigen::Dynamic, 1> > U_data_map((std::complex<double>*)U_data, U.size());
+	
+			// Setup FFT computations
+			fftw_plan fftw_plan = fftw_plan_dft_c2r_1d(2 * (U.size() - 1), U_data, A_data, FFTW_ESTIMATE);
+
+			// Compute inverse FFT
+			U_data_map = U;
+			fftw_execute(fftw_plan);
+			Eigen::VectorXd A = A_data_map;
+			A /= A.size();
+
+			// Free ressources	
+			fftw_destroy_plan(fftw_plan);
+			fftw_free(U_data);
+			fftw_free(A_data);
+
+			// Job done
+			return A;
+		}
+
 		static Eigen::MatrixXcd
 		real_fft_2d(const Eigen::MatrixXd& A) {
 			// Allocate ressources
