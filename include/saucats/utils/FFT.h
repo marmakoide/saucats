98ddd5e2e87c3690725706e846c1d35016f7edb0
Alexandre Devert <alex_devert@adagene.cn>
2019-03-13 11:41:54 +0100

    Refactored FFT code to allow recomputing FFTW plans

--- include/saucats/utils/FFT.h
+++ include/saucats/utils/FFT.h
@@ -3,33 +3,229 @@
 
 #include <fftw3.h>
 #include <Eigen/Dense>
+#include <unsupported/Eigen/CXX11/Tensor>
 
 
 
 namespace saucats {
+	static void
+	fftshift_rows(Eigen::MatrixXcd& X) {
+		for(Eigen::Index i = 0; i < X.rows() / 2; ++i) {
+			Eigen::Index ip = i + (X.rows() / 2);
+			for(Eigen::Index j = 0; j < X.cols(); ++j)
+				std::swap(X(i, j), X(ip, j));
+		}
+	}
+
+	static void
+	fftshift_cols(Eigen::MatrixXcd& X) {
+		for(Eigen::Index i = 0; i < X.rows(); ++i) {
+			for(Eigen::Index j = 0; j < X.cols() / 2; ++j)
+				std::swap(X(i, j), X(i, j + X.cols() / 2));
+			}
+	}
+
+
+
 	/*
 	 * FFT routines, interfacing with the fftw3 library
 	 */
 
+	class FFT1d {
+	public:
+		typedef Eigen::Map<Eigen::Matrix<std::complex<double>, Eigen::Dynamic, 1> > mapping_type;
+
+
+
+		FFT1d(Eigen::Index size) :
+			m_A_data(allocate_array(size)),
+			m_U_data(allocate_array(size)),
+			m_A_map((std::complex<double>*)m_A_data, size),
+			m_U_map((std::complex<double>*)m_U_data, size)
+		{
+			m_fwd_plan = fftw_plan_dft_1d(size, m_A_data, m_U_data, FFTW_FORWARD, FFTW_ESTIMATE);		
+			m_bwd_plan = fftw_plan_dft_1d(size, m_U_data, m_A_data, FFTW_BACKWARD, FFTW_ESTIMATE);
+		}
+
+		~FFT1d() {
+			fftw_destroy_plan(m_fwd_plan);
+			fftw_destroy_plan(m_bwd_plan);
+			fftw_free(m_U_data);
+			fftw_free(m_A_data);
+		}
+
+		mapping_type&
+		fft(const Eigen::VectorXcd& A) {
+			m_A_map = A;
+			fftw_execute(m_fwd_plan);
+			return m_U_map;
+		}
+
+		mapping_type&
+		ifft(const Eigen::VectorXcd& U) {
+			m_U_map = U;
+			fftw_execute(m_bwd_plan);
+			return m_A_map;
+		}
+
+	private:
+		static fftw_complex*
+		allocate_array(Eigen::Index size) {
+			return (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * size);
+		}
+
+
+
+		fftw_complex* m_A_data;
+		fftw_complex* m_U_data;
+		fftw_plan m_fwd_plan;
+		fftw_plan m_bwd_plan;
+		mapping_type m_A_map;
+		mapping_type m_U_map;
+	}; // class FFT1d
+
+
+
+	class FFT2d {
+	public:
+		typedef Eigen::Map<Eigen::Matrix<std::complex<double>, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> > mapping_type;
+
+
+
+		FFT2d(Eigen::Index size) :
+			m_A_data(allocate_array(size)),
+			m_U_data(allocate_array(size)),
+			m_A_map((std::complex<double>*)m_A_data, size, size),
+			m_U_map((std::complex<double>*)m_U_data, size, size)
+		{
+			m_fwd_plan = fftw_plan_dft_2d(size, size, m_A_data, m_U_data, FFTW_FORWARD, FFTW_ESTIMATE);		
+			m_bwd_plan = fftw_plan_dft_2d(size, size, m_U_data, m_A_data, FFTW_BACKWARD, FFTW_ESTIMATE);
+		}
+
+		~FFT2d() {
+			fftw_destroy_plan(m_fwd_plan);
+			fftw_destroy_plan(m_bwd_plan);
+			fftw_free(m_U_data);
+			fftw_free(m_A_data);
+		}
+
+		mapping_type&
+		fft(const Eigen::MatrixXcd& A) {
+			m_A_map = A;
+			fftw_execute(m_fwd_plan);
+			return m_U_map;
+		}
+
+		mapping_type&
+		ifft(const Eigen::MatrixXcd& U) {
+			m_U_map = U;
+			fftw_execute(m_bwd_plan);
+			return m_A_map;
+		}
+
+	private:
+		static fftw_complex*
+		allocate_array(Eigen::Index size) {
+			return (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * size * size);
+		}
+
+
+
+		fftw_complex* m_A_data;
+		fftw_complex* m_U_data;
+		fftw_plan m_fwd_plan;
+		fftw_plan m_bwd_plan;
+		mapping_type m_A_map;
+		mapping_type m_U_map;
+	}; // class FFT2d
+
+
+
+	class RealFFT2d {
+	public:
+		typedef Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> > real_mapping_type;
+		typedef Eigen::Map<Eigen::Matrix<std::complex<double>, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> > complex_mapping_type;
+
+
+
+		RealFFT2d(Eigen::Index size) :
+			m_A_data(allocate_real_array(size)),
+			m_U_data(allocate_complex_array(size)),
+			m_A_map(m_A_data, size, size),
+			m_U_map((std::complex<double>*)m_U_data, size, 1 + size / 2)
+		{
+			m_fwd_plan = fftw_plan_dft_r2c_2d(size, size, m_A_data, m_U_data, FFTW_ESTIMATE);
+			m_bwd_plan = fftw_plan_dft_c2r_2d(size, size, m_U_data, m_A_data, FFTW_ESTIMATE);
+		}
+
+		~RealFFT2d() {
+			fftw_destroy_plan(m_fwd_plan);
+			fftw_destroy_plan(m_bwd_plan);
+			fftw_free(m_U_data);
+			fftw_free(m_A_data);
+		}
+
+		complex_mapping_type&
+		fft(const Eigen::MatrixXd& A) {
+			m_A_map = A;
+			fftw_execute(m_fwd_plan);
+			return m_U_map;
+		}
+
+		real_mapping_type&
+		ifft(const Eigen::MatrixXcd& U) {
+			m_U_map = U;
+			fftw_execute(m_bwd_plan);
+			return m_A_map;
+		}
+
+	private:
+		static double*
+		allocate_real_array(Eigen::Index size) {
+			return (double*)fftw_malloc(sizeof(double) * size * size);
+		}
+
+		static fftw_complex*
+		allocate_complex_array(Eigen::Index size) {
+			return (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * size * (1 + size / 2));
+		}
+
+
+
+		double* m_A_data;
+		fftw_complex* m_U_data;
+		fftw_plan m_fwd_plan;
+		fftw_plan m_bwd_plan;
+		real_mapping_type m_A_map;
+		complex_mapping_type m_U_map;
+	}; // class RealFFT2d
+
+
+
 	class FFT {
 	public:
-		static Eigen::VectorXcd
-		fft_1d(const Eigen::VectorXcd& A) {
+		/*
+		typedef Eigen::Tensor<std::complex<double>, 3, Eigen::RowMajor> tensor3d_complex_type;
+
+		static tensor3d_complex_type
+		fft_3d(const tensor3d_complex_type& A) {
+			Eigen::Index data_size = A.dimension(0) * A.dimension(1) * A.dimension(2);
+
 			// Allocate ressources
-			fftw_complex* A_data = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * A.size());
-			fftw_complex* U_data = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * A.size());
+			fftw_complex* A_data = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * data_size);
+			fftw_complex* U_data = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * data_size);
 
 			// Mapping of raw data for Eigen library
-			Eigen::Map<Eigen::Matrix<std::complex<double>, Eigen::Dynamic, 1> > A_data_map((std::complex<double>*)A_data, A.size());
-			Eigen::Map<Eigen::Matrix<std::complex<double>, Eigen::Dynamic, 1> > U_data_map((std::complex<double>*)U_data, A.size());
+			Eigen::Map<tensor3d_complex_type> A_data_map((std::complex<double>*)A_data, A.dimension(0), A.dimension(1), A.dimension(2));
+			Eigen::Map<tensor3d_complex_type> U_data_map((std::complex<double>*)U_data, A.dimension(0), A.dimension(1), A.dimension(2));
 	
 			// Setup FFT computations
-			fftw_plan fftw_plan = fftw_plan_dft_1d(A.size(), A_data, U_data, FFTW_FORWARD, FFTW_ESTIMATE);
+			fftw_plan fftw_plan = fftw_plan_dft_3d(A.dimension(0), A.dimension(1), A.dimension(2), A_data, U_data, FFTW_FORWARD, FFTW_ESTIMATE);
 
 			// Compute FFT
 			A_data_map = A;
 			fftw_execute(fftw_plan);
-			Eigen::VectorXcd U = U_data_map;
+			tensor3d_complex_type U = U_data_map;
 
 			// Free ressources	
 			fftw_destroy_plan(fftw_plan);
@@ -39,213 +235,7 @@ namespace saucats {
 			// Job done
 			return U;
 		}
-
-
-
-		static Eigen::VectorXcd
-		ifft_1d(const Eigen::VectorXcd& U) {
-			// Allocate ressources
-			fftw_complex* A_data = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * U.size());
-			fftw_complex* U_data = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * U.size());	
-
-			// Mapping of raw data for Eigen library
-			Eigen::Map<Eigen::Matrix<std::complex<double>, Eigen::Dynamic, 1> > A_data_map((std::complex<double>*)A_data, U.size());
-			Eigen::Map<Eigen::Matrix<std::complex<double>, Eigen::Dynamic, 1> > U_data_map((std::complex<double>*)U_data, U.size());
-	
-			// Setup FFT computations
-			fftw_plan fftw_plan = fftw_plan_dft_1d(U.size(), U_data, A_data, FFTW_BACKWARD, FFTW_ESTIMATE);
-
-			// Compute inverse FFT
-			U_data_map = U;
-			fftw_execute(fftw_plan);
-			Eigen::VectorXcd A = A_data_map;
-			A /= A.size();
-
-			// Free ressources	
-			fftw_destroy_plan(fftw_plan);
-			fftw_free(U_data);
-			fftw_free(A_data);
-
-			// Job done
-			return A;
-		}
-
-
-
-		static Eigen::MatrixXcd
-		fft_2d(const Eigen::MatrixXcd& A) {
-			// Allocate ressources
-			fftw_complex* A_data = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * A.rows() * A.cols());
-			fftw_complex* U_data = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * A.rows() * A.cols());
-
-			// Mapping of raw data for Eigen library
-			Eigen::Map<Eigen::Matrix<std::complex<double>, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> > A_data_map((std::complex<double>*)A_data, A.rows(), A.cols());
-			Eigen::Map<Eigen::Matrix<std::complex<double>, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> > U_data_map((std::complex<double>*)U_data, A.rows(), A.cols());
-	
-			// Setup FFT computations
-			fftw_plan fftw_plan = fftw_plan_dft_2d(A.cols(), A.rows(), A_data, U_data, FFTW_FORWARD, FFTW_ESTIMATE);
-
-			// Compute FFT
-			A_data_map = A;
-			fftw_execute(fftw_plan);
-			Eigen::MatrixXcd U = U_data_map;
-
-			// Free ressources	
-			fftw_destroy_plan(fftw_plan);
-			fftw_free(U_data);
-			fftw_free(A_data);
-
-			// Job done
-			return U;
-		}
-
-
-
-		static Eigen::MatrixXcd
-		ifft_2d(const Eigen::MatrixXcd& U) {
-			// Allocate ressources
-			fftw_complex* A_data = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * U.rows() * U.cols());
-			fftw_complex* U_data = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * U.rows() * U.cols());
-
-			// Mapping of raw data for Eigen library
-			Eigen::Map<Eigen::Matrix<std::complex<double>, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> > A_data_map((std::complex<double>*)A_data, U.rows(), U.cols());
-			Eigen::Map<Eigen::Matrix<std::complex<double>, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> > U_data_map((std::complex<double>*)U_data, U.rows(), U.cols());
-	
-			// Setup FFT computations
-			fftw_plan fftw_plan = fftw_plan_dft_2d(U.cols(), U.rows(), U_data, A_data, FFTW_BACKWARD, FFTW_ESTIMATE);
-
-			// Compute inverse FFT
-			U_data_map = U;
-			fftw_execute(fftw_plan);
-			Eigen::MatrixXcd A = A_data_map;
-			A /= A.rows() * A.cols();
-
-			// Free ressources	
-			fftw_destroy_plan(fftw_plan);
-			fftw_free(U_data);
-			fftw_free(A_data);
-
-			// Job done
-			return A;
-		}
-
-
-
-		static Eigen::VectorXcd
-		real_fft_1d(const Eigen::VectorXd& A) {
-			// Allocate ressources
-			double* A_data = (double*)fftw_malloc(sizeof(double) * A.size());
-			fftw_complex* U_data = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * (1 + A.size() / 2));
-
-			// Mapping of raw data for Eigen library
-			Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, 1> > A_data_map(A_data, A.size());
-			Eigen::Map<Eigen::Matrix<std::complex<double>, Eigen::Dynamic, 1> > U_data_map((std::complex<double>*)U_data, 1 + A.size() / 2);
-	
-			// Setup FFT computations
-			fftw_plan fftw_plan = fftw_plan_dft_r2c_1d(A.size(), A_data, U_data, FFTW_ESTIMATE);
-
-			// Compute FFT
-			A_data_map = A;
-			fftw_execute(fftw_plan);
-			Eigen::VectorXcd U = U_data_map;
-
-			// Free ressources	
-			fftw_destroy_plan(fftw_plan);
-			fftw_free(U_data);
-			fftw_free(A_data);
-
-			// Job done
-			return U;
-		}
-
-
-
-		static Eigen::VectorXd
-		real_ifft_1d(const Eigen::VectorXcd& U) {
-			// Allocate ressources
-			double* A_data = (double*)fftw_malloc(sizeof(double) * (2 * (U.size() - 1)));
-			fftw_complex* U_data = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * U.size());
-
-			// Mapping of raw data for Eigen library
-			Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, 1> > A_data_map(A_data, 2 * (U.size() - 1));
-			Eigen::Map<Eigen::Matrix<std::complex<double>, Eigen::Dynamic, 1> > U_data_map((std::complex<double>*)U_data, U.size());
-	
-			// Setup FFT computations
-			fftw_plan fftw_plan = fftw_plan_dft_c2r_1d(2 * (U.size() - 1), U_data, A_data, FFTW_ESTIMATE);
-
-			// Compute inverse FFT
-			U_data_map = U;
-			fftw_execute(fftw_plan);
-			Eigen::VectorXd A = A_data_map;
-			A /= A.size();
-
-			// Free ressources	
-			fftw_destroy_plan(fftw_plan);
-			fftw_free(U_data);
-			fftw_free(A_data);
-
-			// Job done
-			return A;
-		}
-
-
-
-		static Eigen::MatrixXcd
-		real_fft_2d(const Eigen::MatrixXd& A) {
-			// Allocate ressources
-			double* A_data = (double*)fftw_malloc(sizeof(double) * A.rows() * A.cols());
-			fftw_complex* U_data = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * A.rows() * (1 + A.cols() / 2));
-
-			// Mapping of raw data for Eigen library
-			Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> > A_data_map(A_data, A.rows(), A.cols());
-			Eigen::Map<Eigen::Matrix<std::complex<double>, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> > U_data_map((std::complex<double>*)U_data, A.rows(), 1 + A.cols() / 2);
-	
-			// Setup FFT computations
-			fftw_plan fftw_plan = fftw_plan_dft_r2c_2d(A.cols(), A.rows(), A_data, U_data, FFTW_ESTIMATE);
-
-			// Compute FFT
-			A_data_map = A;
-			fftw_execute(fftw_plan);
-			Eigen::MatrixXcd U = U_data_map;
-
-			// Free ressources	
-			fftw_destroy_plan(fftw_plan);
-			fftw_free(U_data);
-			fftw_free(A_data);
-
-			// Job done
-			return U;
-		}
-
-
-
-		static Eigen::MatrixXd
-		real_ifft_2d(const Eigen::MatrixXcd& U) {
-			// Allocate ressources
-			double* A_data = (double*)fftw_malloc(sizeof(double) * U.rows() * (2 * (U.cols() - 1)));
-			fftw_complex* U_data = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * U.rows() * U.cols());
-
-			// Mapping of raw data for Eigen library
-			Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> > A_data_map(A_data, U.rows(), 2 * (U.cols() - 1));
-			Eigen::Map<Eigen::Matrix<std::complex<double>, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> > U_data_map((std::complex<double>*)U_data, U.rows(), U.cols());
-	
-			// Setup FFT computations
-			fftw_plan fftw_plan = fftw_plan_dft_c2r_2d(2 * (U.cols() - 1), U.rows(), U_data, A_data, FFTW_ESTIMATE);
-
-			// Compute inverse FFT
-			U_data_map = U;
-			fftw_execute(fftw_plan);
-			Eigen::MatrixXd A = A_data_map;
-			A /= A.rows() * A.cols();
-
-			// Free ressources	
-			fftw_destroy_plan(fftw_plan);
-			fftw_free(U_data);
-			fftw_free(A_data);
-
-			// Job done
-			return A;
-		}
+		*/
 	}; // class FFT
 } // namespace saucats
 
--- include/saucats/utils/PseudoPolarFFT.h
+++ include/saucats/utils/PseudoPolarFFT.h
@@ -7,118 +7,107 @@
 
 
 namespace saucats {
-	class PseudoPolarFFT {
+	class PseudoPolarFFT2d {
 	public:
-		static Eigen::VectorXcd
-		frft(const Eigen::VectorXcd& A, double alpha) {
-			const std::complex<double> j(0., 1.);
-    
-			Eigen::Index N = A.size();
-		
-			Eigen::VectorXcd K(2 * N);
-			for(Eigen::Index i = 0; i < N; ++i) {
-				K[i] = i;
-				K[2 * N - 1 - i] = -(i + 1); 
-			}
-			K = ((-M_PI * alpha * j) * K.array().square()).exp();
-
-			Eigen::VectorXcd X(2 * N);
-			X.head(N) = A;
-			X.tail(N) = Eigen::VectorXcd::Zero(N);
-			X.array() *= K.array();
-
-			Eigen::VectorXcd XX = FFT::fft_1d(X);
-			XX.array() *= FFT::fft_1d(K.conjugate()).array();
-			Eigen::VectorXcd Y = FFT::ifft_1d(XX);
-			Y.array() *= K.array();
-
-			return Y.head(N);
-		}
+		PseudoPolarFFT2d(Eigen::Index size) :
+			m_fft_1d(2 * size),
+			m_N(size) { } 
 
 
 
-		static Eigen::VectorXcd
-		centered_frft(const Eigen::VectorXcd& A, double alpha) {
+		Eigen::MatrixXcd
+		operator () (const Eigen::MatrixXd& A) {
 			const std::complex<double> j(0., 1.);
 
-			Eigen::Index N = A.size();
+			double N2 = m_N * m_N;
 
-			Eigen::VectorXcd K(N);
-			for(Eigen::Index i = 0; i < N; ++i)
-				K[i] = i;
-			K *= M_PI * alpha * N * j;
-			
-			Eigen::VectorXcd Ap = A.array() * K.array().exp();
-			return frft(Ap, alpha);
-		}
-
-		
-
-		static void
-		fftshift_rows(Eigen::MatrixXcd& X) {
-			for(Eigen::Index i = 0; i < X.rows() / 2; ++i) {
-				Eigen::Index ip = i + (X.rows() / 2);
-				for(Eigen::Index j = 0; j < X.cols(); ++j)
-					std::swap(X(i, j), X(ip, j));
-			}
-		}
-
-		static void
-		fftshift_cols(Eigen::MatrixXcd& X) {
-			for(Eigen::Index i = 0; i < X.rows(); ++i) {
-				for(Eigen::Index j = 0; j < X.cols() / 2; ++j)
-					std::swap(X(i, j), X(i, j + X.cols() / 2));
-			}
-		}
-
-
-		static Eigen::MatrixXcd
-		pseudo_polar_fft_2d(const Eigen::MatrixXd& A) {
-			const std::complex<double> j(0., 1.);
-
-			Eigen::Index N = A.rows();
-			double N2 = N * N;
-
-			Eigen::MatrixXcd ret(2 * N, 2 * N);
+			Eigen::MatrixXcd ret(2 * m_N, 2 * m_N);
 			ret.array() = 0.;
 
 			// Construction of quadrant 1 and 3
       {
-				Eigen::MatrixXcd Ap(2 * N, N);
-				Ap.topRows(N) = A;
-				Ap.bottomRows(N).array() = 0.;
+				Eigen::MatrixXcd Ap(2 * m_N, m_N);
+				Ap.topRows(m_N) = A;
+				Ap.bottomRows(m_N).array() = 0.;
 
-				Eigen::MatrixXcd U(2 * N, N);
-				for(Eigen::Index i = 0; i < N; ++i)
-					U.col(i) = FFT::fft_1d(Ap.col(i));
+				Eigen::MatrixXcd U(2 * m_N, m_N);
+				for(Eigen::Index i = 0; i < m_N; ++i)
+					U.col(i) = m_fft_1d.fft(Ap.col(i));
+				
 				fftshift_rows(U);
 
-				for(Eigen::Index i = -N; i < N; ++i)
-					ret.row(i+N).head(N) = centered_frft(U.row(i+N), i / N2).reverse();
+				for(Eigen::Index i = -m_N; i < m_N; ++i)
+					ret.row(i+m_N).head(m_N) = centered_frft(U.row(i+m_N), i / N2).reverse();
 			}
 
 			// Construction of quadrant 2 and 4
 			{
-				Eigen::MatrixXcd Ap(N, 2 * N);
-				Ap.leftCols(N) = A;
-				Ap.rightCols(N).array() = 0.;
+				Eigen::MatrixXcd Ap(m_N, 2 * m_N);
+				Ap.leftCols(m_N) = A;
+				Ap.rightCols(m_N).array() = 0.;
 
-				Eigen::MatrixXcd U(N, 2 * N);
-				for(Eigen::Index i = 0; i < N; ++i)
-					U.row(i) = FFT::fft_1d(Ap.row(i));
+				Eigen::MatrixXcd U(m_N, 2 * m_N);
+				for(Eigen::Index i = 0; i < m_N; ++i)
+					U.row(i) = m_fft_1d.fft(Ap.row(i));
+
 				fftshift_cols(U);
 				U.transposeInPlace();
 
-				for(Eigen::Index i = -N; i < N; ++i) {
-					Eigen::VectorXcd K = (((N / 2 - 1) * (i / (N2)) * 2 * M_PI * j) * Eigen::VectorXcd::LinSpaced(N, 0., N - 1)).array().exp();
-					K.array() *= U.row(i+N).array();
-					ret.row(i+N).tail(N) = frft(K, i / N2);			
+				for(Eigen::Index i = -m_N; i < m_N; ++i) {
+					Eigen::VectorXcd K = (((m_N / 2 - 1) * (i / (N2)) * 2 * M_PI * j) * Eigen::VectorXcd::LinSpaced(m_N, 0., m_N - 1)).array().exp();
+					K.array() *= U.row(i+m_N).array();
+					ret.row(i+m_N).tail(m_N) = frft(K, i / N2);			
 				}
 			}
 
 			// Job done
 			return ret;
 		}
+
+	private:
+		Eigen::VectorXcd
+		frft(const Eigen::VectorXcd& A, double alpha) {
+			const std::complex<double> j(0., 1.);
+		
+			Eigen::VectorXcd K(2 * m_N);
+			for(Eigen::Index i = 0; i < m_N; ++i) {
+				K[i] = i;
+				K[2 * m_N - 1 - i] = -(i + 1); 
+			}
+			K = ((-M_PI * alpha * j) * K.array().square()).exp();
+
+			Eigen::VectorXcd X(2 * m_N);
+			X.head(m_N) = A;
+			X.tail(m_N) = Eigen::VectorXcd::Zero(m_N);
+			X.array() *= K.array();
+
+			Eigen::VectorXcd XX = m_fft_1d.fft(X);
+			XX.array() *= m_fft_1d.fft(K.conjugate()).array();
+			Eigen::VectorXcd Y = m_fft_1d.ifft(XX);
+			Y.array() *= K.array();
+
+			return Y.head(m_N);
+		}
+
+
+
+		Eigen::VectorXcd
+		centered_frft(const Eigen::VectorXcd& A, double alpha) {
+			const std::complex<double> j(0., 1.);
+
+			Eigen::VectorXcd K(m_N);
+			for(Eigen::Index i = 0; i < m_N; ++i)
+				K[i] = i;
+			K *= M_PI * alpha * m_N * j;
+			
+			Eigen::VectorXcd Ap = A.array() * K.array().exp();
+			return frft(Ap, alpha);
+		}
+
+
+
+		FFT1d m_fft_1d;
+		Eigen::Index m_N;		
 	}; // class PseudoPolarFFT
 } // namespace saucats
 
